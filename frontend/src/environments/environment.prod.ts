1652b0a971149854c7a4bfea2a0e06a3d6035df3
Johannduranki <johannvv@duranki.network>
2026-08-25 20:39:55 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com' // Update this with your production API URL
+  apiUrl: 'http://app-dev.durankidemo.network:8080' // Update this with your production API URL
 };
